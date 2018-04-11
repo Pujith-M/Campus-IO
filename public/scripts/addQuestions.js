528e23348f3542ddafdf9f6a40ed538a1c9e388b
Pujith-M <pujith.m95@gmail.com>
2018-04-11 22:56:54 +0530

    Done with Adding quiz

--- public/scripts/addQuestions.js
+++ public/scripts/addQuestions.js
@@ -1,17 +1,47 @@
-var questionContainer = document.getElementById("questionContainer");
+var optionContainer = document.getElementById("optionContainer");
 var addOptions = document.getElementById("addOptions");
-function addTextField() {
-	questionContainer.appendChild(document.createTextNode("hahhaha"));
-	var input = document.createElement("input");
-	input.type = "text";
-	input.name = "member";
-	input.class="form-control";
-	input.id="exampleFormControlTextarea1";
-	input.rows="3";
-	questionContainer.appendChild(input);
-	// Append a line break 
-	questionContainer.appendChild(document.createElement("br"));
+
+var optionCounter = 1;
+
+function addTextField(questionNum) {
+	var divUpperElement = document.createElement("div");
+	var divInnerElement = document.createElement("div");
+	var lableElement = document.createElement("Label");
+	var textareaElement = document.createElement("textarea");
+	var radioButton = document.createElement("input");
+
+	radioButton.type = "radio";
+	radioButton.name = "answer";
+	radioButton.required = "true";
+	radioButton.id = "q156";
+	radioButton.value = "option" + questionNum;
+
+	divInnerElement.className = "input-group-prepend input-group-text";
+	divUpperElement.className = "input-group";
+	divInnerElement.appendChild(radioButton);
+	divUpperElement.appendChild(divInnerElement);
+
+
+	lableElement.setAttribute("for", "option" + questionNum);
+	lableElement.appendChild(document.createTextNode("Option" + questionNum));
+	optionContainer.appendChild(lableElement);
+	
+	textareaElement.type = "text";
+	textareaElement.required = "true";
+	textareaElement.name = "option" + questionNum;
+	textareaElement.className="form-control";
+	textareaElement.id="exampleFormControlTextarea1";
+	textareaElement.rows="1";
+	divUpperElement.appendChild(textareaElement);
+
+	optionContainer.appendChild(divUpperElement);
+	optionContainer.appendChild(document.createElement("br"));
 }
+addTextField(optionCounter++);
+addTextField(optionCounter++);
+
 addOptions.addEventListener('click', function() {
-	addTextField();
+	addTextField(optionCounter++);
+	if(optionCounter > 5)
+		addOptions.style.display = "none";
 });
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,8 @@
 // requiring packages
 var express		=	require('express'),
-	router		=	express.Router(),
-	Quiz		=	require('../models/quiz'),
-	middleware 	=	require('../middleware');
+router		=	express.Router(),
+Quiz		=	require('../models/quiz'),
+middleware 	=	require('../middleware');
 
 // Crating new quiz
 router.get('/create', middleware.isLoggedIn, function(req, res) {
@@ -10,49 +10,53 @@ router.get('/create', middleware.isLoggedIn, function(req, res) {
 });
 
 // Adding questions to quiz
-router.get('/addQuestions/:quizId', middleware.isLoggedIn, function(req, res) {
-	res.render('quiz/addQuestions', {
-		quizId : req.params.quizId,
-		questionNo : 4//Quiz.findOne(req.params.quizId).questions.length
+// router.get('/addQuestions/:quizId', middleware.isLoggedIn, function(req, res) {
+router.get('/addQuestions/:quizId', function(req, res) {
+	Quiz.findById(req.params.quizId, function(error, foundQuiz) {
+		if(error)
+			req.flash('error', 'Something went wrong. Please try again.');
+		else{
+			if(foundQuiz.author.id.equals(req.user._id)){
+				res.render('quiz/addQuestions', {
+					quizId : req.params.quizId,
+					questionNo : foundQuiz.questions.length + 1
+				});
+			}
+			else
+				res.redirect('/user/home');
+		}
 	});
 });
 
 // Adding the question to the quizz
-router.post('/addQuestions/:quizId', middleware.isLoggedIn, function(req, res) {
-	var newQuestion = {
-		question : req.body.question,
-		options : [
-		{
-			option : req.body.option1
-		},
-		{
-			option : req.body.option2
-		},	
-		{
-			option : req.body.option3
-		},		
-		{
-			option : req.body.option4
-		}],
-		answer : req.body.answer
-	};
-	Quiz.update(
-		{ _id: req.params.quizId },
-		{ $push: { questions: newQuestion } },
-		function(err, num) {});
+// router.post('/addQuestions/:quizId', middleware.isLoggedIn, function(req, res) {
+	router.post('/addQuestions/:quizId', function(req, res) {
+		var newQuestion = {
+			question : req.body.question,
+			answer : req.body[req.body.answer],
+			options : []
+		};
+		for(var i=1;i<Object.keys(req.body).length - 1;i++){
+			newQuestion.options.push({ option : req.body["option" + i]});
+		}
+		console.log(newQuestion);
+		Quiz.update(
+			{ _id: req.params.quizId },
+			{ $push: { questions: newQuestion } },
+			function(err, num) {});
 		
-	res.redirect('/quiz/addQuestions/' + req.params.quizId);
-});
+		res.redirect('/quiz/addQuestions/' + req.params.quizId);
+	});
 
 
 // Adding the title to the quizz
 router.post('/create', middleware.isLoggedIn, function(req, res) {
 	var newQuiz = new Quiz({
-			title : req.body.title,
-			author : {
-				id : req.user._id,
-				username : req.user.username
-			}
+		title : req.body.title,
+		author : {
+			id : req.user._id,
+			username : req.user.username
+		}
 	});
 	Quiz.create(newQuiz,function(err, newlyCreated){
 		if(err) {
